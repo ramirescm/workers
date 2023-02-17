67fca2be4e511cfaba827e22fdb5c78eaf9c482a
ramires marques <ramiresmarques@ramirescm.local>
2023-02-17 20:59:01 -0300

    initial commit

---
+++ workers/Program.cs
@@ -0,0 +1,11 @@
+﻿using workers;
+
+IHost host = Host.CreateDefaultBuilder(args)
+    .ConfigureServices(services =>
+    {
+        services.AddHostedService<Worker>();
+    })
+    .Build();
+
+host.Run();
+
